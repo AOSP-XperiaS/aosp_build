10a75613af1639a953f5c35f1db97435c78e383b
Apology11 <apology11@gmail.com>
2014-06-13 00:20:48 +0200

    fix indentation

--- tools/releasetools/edify_generator.py
+++ tools/releasetools/edify_generator.py
@@ -92,7 +92,7 @@ class EdifyGenerator(object):
 
   def AssertDevice(self, device):
     """Assert that the device identifier is the given string."""
-     cmd = ('assert(' +
+    cmd = ('assert(' +
            ' || \0'.join(['getprop("ro.product.device") == "%s" || getprop("ro.build.product") == "%s"'
                          % (i, i) for i in device.split(",")]) +
            ' || abort("This package is for \\"%s\\" devices; '
